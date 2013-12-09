046fdc2f2b175666eb460b3a38a54573919ed34e
vitorecomp <vitor.ecomp@gmail.com>
2013-12-09 02:21:01 -0200

    Add basetypes

---
+++ nos/simple-embedded-systems/master/BaseTypes.h
@@ -0,0 +1,32 @@
+#ifndef BASE_TYPE
+#define BASE_TYPE
+
+#include <string.h>
+
+class Interrupt
+{
+private:
+	string service;
+public:
+	Interrupt(string)
+	void getService();
+};
+
+Interrupt::Interrupt(string service)
+{
+	this->service = service;
+}
+
+string Interrupt::getService()
+{
+	return this->service;
+}
+
+class Service
+{
+private:
+	string service;
+
+};
+
+#endif
